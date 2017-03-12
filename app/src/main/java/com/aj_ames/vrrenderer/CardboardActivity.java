72f693890ab9b04f0828aad2a27bc798086d26b9
aj-ames <akash.james2096@gmail.com>
2017-03-13 02:32:50 +0530

    Add: Cardboard UI using FAB

--- app/src/main/java/com/aj_ames/vrrenderer/CardboardActivity.java
+++ app/src/main/java/com/aj_ames/vrrenderer/CardboardActivity.java
@@ -3,11 +3,73 @@ package com.aj_ames.vrrenderer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class CardboardActivity extends AppCompatActivity {
+import com.google.vr.sdk.base.AndroidCompat;
+import com.google.vr.sdk.base.Eye;
+import com.google.vr.sdk.base.GvrActivity;
+import com.google.vr.sdk.base.GvrView;
+import com.google.vr.sdk.base.HeadTransform;
+import com.google.vr.sdk.base.Viewport;
+
+import javax.microedition.khronos.egl.EGLConfig;
+
+public class CardboardActivity extends GvrActivity implements GvrView.StereoRenderer{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cardboard);
+        initializeGvrView();
+    }
+    public void initializeGvrView() {
+        setContentView(R.layout.activity_cardboard);
+
+        GvrView gvrView = (GvrView) findViewById(R.id.gvr_view);
+        gvrView.setEGLConfigChooser(8, 8, 8, 8, 16, 8);
+
+        gvrView.setRenderer(this);
+        gvrView.setTransitionViewEnabled(true);
+
+        // Enable Cardboard-trigger feedback with Daydream headsets. This is a simple way of supporting
+        // Daydream controller input for basic interactions using the existing Cardboard trigger API.
+        gvrView.enableCardboardTriggerEmulation();
+
+        if (gvrView.setAsyncReprojectionEnabled(true)) {
+            // Async reprojection decouples the app framerate from the display framerate,
+            // allowing immersive interaction even at the throttled clockrates set by
+            // sustained performance mode.
+            AndroidCompat.setSustainedPerformanceMode(this, true);
+        }
+
+        setGvrView(gvrView);
+    }
+
+    @Override
+    public void onNewFrame(HeadTransform headTransform) {
+
+    }
+
+    @Override
+    public void onDrawEye(Eye eye) {
+
+    }
+
+    @Override
+    public void onFinishFrame(Viewport viewport) {
+
+    }
+
+    @Override
+    public void onSurfaceChanged(int i, int i1) {
+
+    }
+
+    @Override
+    public void onSurfaceCreated(EGLConfig eglConfig) {
+
+    }
+
+    @Override
+    public void onRendererShutdown() {
+
     }
 }
